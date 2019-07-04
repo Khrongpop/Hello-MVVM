879023b90b15a0ac7ed3333e74b6150acbcfd24e
Muang <mr.khrongpop@gmail.com>
2019-07-04 10:19:06 +0700

    Initial Commit

---
+++ Hello-MVVM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hello-MVVM
+//
+//  Created by Muang on 4/7/2562 BE.
+//  Copyright © 2562 khrongpop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
